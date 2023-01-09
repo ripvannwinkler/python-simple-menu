2343b8eee6a6b6f5bc6cef969f403b9203bb1dea
semantic-release <semantic-release>
2023-01-08 22:51:14 -0600

    0.1.0
    Automatically generated by python-semantic-release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-VERSION = "0.0.1"
+VERSION = "0.1.0"
 
 setup(
     name="simple_menu",
@@ -9,12 +9,15 @@ setup(
     author="Chris Vann",
     author_email="chrisvann01@gmail.com",
     description="A simple console menu",
+    url="https://github.com/ripvannwinkler/python-simple-menu",
     packages=find_packages(),
     install_requires=[],
     python_requires=">=3.7",
     keywords=["python", "menu", "simple menu", "console menu"],
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
     ],
 )
